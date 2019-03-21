0908b829b9ceebcf361f7d83a7093fe24fc87faa
David Svensson <david.svensson@nackademin.se>
2019-03-21 09:15:00 +0100

    First commit

---
+++ MAP18_GesturePainterTests/MAP18_GesturePainterTests.swift
@@ -0,0 +1,34 @@
+//
+//  MAP18_GesturePainterTests.swift
+//  MAP18_GesturePainterTests
+//
+//  Created by David Svensson on 2019-03-21.
+//  Copyright © 2019 David Svensson. All rights reserved.
+//
+
+import XCTest
+@testable import MAP18_GesturePainter
+
+class MAP18_GesturePainterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
